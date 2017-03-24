fc8c9a41e1d816ed620ba9b64ea8731c1e8b24f9
Daniel Beckert <drbeckert@gmail.com>
2017-03-23 23:26:28 -0300

    Initial menu structure

--- client-app/src/app/menu/menu.component.ts
+++ client-app/src/app/menu/menu.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 export class MenuItem {
-  label:string;
-  path :string;
+  id    :number;
+  label :string;
+  path  :string;
+  icon  :string;
+  active:boolean;
 }
 
 @Component({
-  selector: 'app-menu',
+  selector: 'menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public menuItems:Array<MenuItem>;
 
+  constructor() {
+    this.menuItems = [
+      {'id': 1, 'label': 'Dashboard', 'path': '#', 'icon': 'dashboard', 'active': true},
+      {'id': 2, 'label': 'Charts', 'path': '#', 'icon': 'pie-chart', 'active': false}
+    ]
+  }
   ngOnInit() {
   }
+  menuItemClick(id:number) {
+    this.menuItems.map((item) => {
+      if(item.id === id) {
+        item.active = true;
+      } else {
+        item.active = false;
+      }
+      return item;
+    });
+  }
+
 
 }
